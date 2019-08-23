d7cb33462680372ca1425e89fc1484c61e46d999
shubhammodi004 <shubhammodi004@example.com>
2019-08-23 14:38:28 +0530

    now change

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -32,7 +32,7 @@ class Loader extends Component {
                     <div className="loader">
                         <Fade top when={show} duration={800}>
                             <div className="loaderLogo">
-                                <img src={require('../../images/logo.png')} />
+                                <img src={require('../../images/Logo.png')} />
                             </div>
                         </Fade>
                     </div>
